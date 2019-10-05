2c359bcc5f88b24f235585efecfbb3a339a9bbb0
Wjer <17909992954@qq.com>
2019-10-05 19:19:18 +0800

    i can't git in terminal

--- day02_machine_learning.py
+++ day02_machine_learning.py
@@ -3,6 +3,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
 
 def knn_iris():
@@ -65,8 +69,68 @@ def knn_iris_gscv():
     return None
 
 
+def nb_news():
+    """
+    用朴素贝叶斯算法对新闻分类
+    :return:
+    """
+    # 1) 获取数据
+    news = fetch_20newsgroups(subset='all')
+
+    # 2) 划分数据集
+    x_train, x_test, y_train, y_test = train_test_split(news.data, news.target)
+
+    # 3) 特征工程 文本特征抽取 tfidf
+    transfer = TfidfVectorizer()
+    x_train = transfer.fit_transform(x_train)
+    x_test = transfer.transform(x_test)
+
+    # 4) 朴素贝叶斯预估器流程
+    estimator = MultinomialNB()
+    estimator.fit(x_train, y_train)
+
+    # 5) 模型评估
+    y_predict = estimator.predict(x_test)
+    print("y_predict:\n", y_predict)
+    print("是否相等：\n", y_test == y_predict)
+    score = estimator.score(x_test, y_test)
+    print("准确率为：\n", score)
+    return None
+
+
+def decision_iris():
+    """
+    用决策树对鸢尾花进行分类
+    :return:
+    """
+    # 1) 获取数据集
+    iris = load_iris()
+
+    # 2) 划分数据集
+    x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=22)
+
+    # 3) 决策树预估器
+    estimator = DecisionTreeClassifier(criterion="entropy")
+    estimator.fit(x_train, y_train)
+
+    # 4) 模型评估
+    y_predict = estimator.predict(x_test)
+    print("y_predict:\n", y_predict)
+    print("是否相等：\n", y_test == y_predict)
+    score = estimator.score(x_test, y_test)
+    print("准确率为：\n", score)
+
+    # 决策树可视化
+    export_graphviz(estimator, out_file="iris_tree.dot", feature_names=iris.feature_names)
+    return None
+
+
 if __name__ == "__main__":
     # KNN
     # knn_iris()
     # 添加网格搜索和交叉验证
-    knn_iris_gscv()
+    # knn_iris_gscv()
+    # 用朴素贝叶斯算法对新闻分类
+    # nb_news()
+    # 用决策树对鸢尾花进行分类
+    decision_iris()
